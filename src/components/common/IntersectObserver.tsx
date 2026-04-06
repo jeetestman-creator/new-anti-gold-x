a2a359c305df54d5c604aa43b2cd200b6ef7b496
jeetestman-creator <jeetestman@gmail.com>
2026-04-07 02:36:30 +0530

    Update IntersectObserver.tsx

--- src/components/common/IntersectObserver.tsx
+++ src/components/common/IntersectObserver.tsx
@@ -1,6 +1,5 @@
 import { useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
-import { Observer } from 'tailwindcss-intersect';
 
 const IntersectObserver = () => {
   const location = useLocation();
